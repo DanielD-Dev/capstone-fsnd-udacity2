b3aa69a379f080abbbd7a8ce48640df438729922
Daniil <ddreyman1@gmail.com>
2023-08-19 13:49:20 -0400

    implement the logic for some of the routes

--- app.py
+++ app.py
@@ -156,17 +156,32 @@ def verify_decode_jwt(token):
             }, 400)
 
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+def requires_auth(permissions):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+                check_permissions(permissions, payload)
+            except AuthError as auth_error:
+                abort(auth_error.status_code, auth_error.error)
+            return f(payload, *args, **kwargs)
+        return wrapper
+    return decorator
 
-    return wrapper
+def check_permissions(permissions, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permissions not included in JWT.'
+        }, 400)
+    
+    if not any(permission in payload['permissions'] for permission in permissions):
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
 
 @app.route('/headers')
 @requires_auth
@@ -186,54 +201,172 @@ def create_app(test_config=None):
     db.init_app(app)
 
     @app.route('/actors', methods=['GET'])
-    @requires_auth('get:actors')
-    def get_actors():
-        # Implement logic to retrieve actors from the database
-        # Return a JSON response with the list of actors
+    @requires_auth(['get:actors'])
+    def get_actors(payload):
+        try:
+            actors = Actor.query.all()  # Assuming Actor is your SQLAlchemy model
+            formatted_actors = [actor.format() for actor in actors]  # Format actors using a method in your model
+            return jsonify({
+                'success': True,
+                'actors': formatted_actors
+            })
+        except Exception:
+            abort(500)
+
     
     @app.route('/movies', methods=['GET'])
     @requires_auth('get:movies')
-    def get_movies():
-        # Implement logic to retrieve movies from the database
-        # Return a JSON response with the list of movies
+    def get_movies(payload):
+    try:
+        movies = Movie.query.all()  # Assuming Movie is your SQLAlchemy model
+        formatted_movies = [movie.format() for movie in movies]  # Format movies using a method in your model
+        return jsonify({
+            'success': True,
+            'movies': formatted_movies
+        })
+    except Exception:
+        abort(500)
     
     @app.route('/actors', methods=['POST'])
-    @requires_auth('post:actors')
-    def create_actor():
-        # Implement logic to create a new actor in the database
-        # Return a JSON response with the newly created actor
+    @requires_auth(['post:actors'])
+    def create_actor(payload):
+        try:
+            data = request.get_json()
+            new_actor = Actor(
+                name=data.get('name'),
+                age=data.get('age'),
+                gender=data.get('gender')
+            )
+            db.session.add(new_actor)
+            db.session.commit()
+            return jsonify({
+                'success': True,
+                'actor': new_actor.format()
+            })
+        except Exception:
+            db.session.rollback()
+            abort(500)
     
     @app.route('/movies', methods=['POST'])
-    @requires_auth('post:movies')
-    def create_movie():
-        # Implement logic to create a new movie in the database
-        # Return a JSON response with the newly created movie
+    @requires_auth(['post:movies'])
+    def create_movie(payload):
+        try:
+            data = request.get_json()
+            new_movie = Movie(
+                title=data.get('title'),
+                release_date=data.get('release_date')
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+            return jsonify({
+                'success': True,
+                'movie': new_movie.format()
+            })
+        except Exception:
+            db.session.rollback()
+            abort(500)
     
     @app.route('/actors/<int:actor_id>', methods=['PATCH'])
-    @requires_auth('patch:actors')
-    def update_actor(actor_id):
-        # Implement logic to update an existing actor in the database
-        # Return a JSON response with the updated actor
+    @requires_auth(['patch:actors'])
+    def update_actor(payload, actor_id):
+        try:
+            actor = Actor.query.get(actor_id)
+            if not actor:
+                abort(404)
+
+            data = request.get_json()
+            if 'name' in data:
+                actor.name = data['name']
+            if 'age' in data:
+                actor.age = data['age']
+            if 'gender' in data:
+                actor.gender = data['gender']
+
+            db.session.commit()
+            return jsonify({
+                'success': True,
+                'actor': actor.format()
+            })
+        except Exception:
+            db.session.rollback()
+            abort(500)
     
     @app.route('/movies/<int:movie_id>', methods=['PATCH'])
-    @requires_auth('patch:movies')
-    def update_movie(movie_id):
-        # Implement logic to update an existing movie in the database
-        # Return a JSON response with the updated movie
+    @requires_auth(['patch:movies'])
+    def update_movie(payload, movie_id):
+        try:
+            movie = Movie.query.get(movie_id)
+            if not movie:
+                abort(404)
+
+            data = request.get_json()
+            if 'title' in data:
+                movie.title = data['title']
+            if 'release_date' in data:
+                movie.release_date = data['release_date']
+
+            db.session.commit()
+            return jsonify({
+                'success': True,
+                'movie': movie.format()
+            })
+        except Exception:
+            db.session.rollback()
+            abort(500)
     
     @app.route('/actors/<int:actor_id>', methods=['DELETE'])
-    @requires_auth('delete:actors')
-    def delete_actor(actor_id):
-        # Implement logic to delete an actor from the database
-        # Return a JSON response indicating success
+    @requires_auth(['delete:actors'])
+    def delete_actor(payload, actor_id):
+        try:
+            actor = Actor.query.get(actor_id)
+            if not actor:
+                abort(404)
+
+            db.session.delete(actor)
+            db.session.commit()
+            return jsonify({
+                'success': True,
+                'message': 'Actor deleted successfully'
+            })
+        except Exception:
+            db.session.rollback()
+            abort(500)
     
     @app.route('/movies/<int:movie_id>', methods=['DELETE'])
-    @requires_auth('delete:movies')
-    def delete_movie(movie_id):
-        # Implement logic to delete a movie from the database
-        # Return a JSON response indicating success
+    @requires_auth(['delete:movies'])
+    def delete_movie(payload, movie_id):
+        try:
+            movie = Movie.query.get(movie_id)
+            if not movie:
+                abort(404)
+
+            db.session.delete(movie)
+            db.session.commit()
+            return jsonify({
+                'success': True,
+                'message': 'Movie deleted successfully'
+            })
+        except Exception:
+            db.session.rollback()
+            abort(500)
     
-    # Implement the @app.errorhandler decorator for formatting error responses
+    # Custom error handler for 422 Unprocessable Entity
+    @app.errorhandler(422)
+    def unprocessable_entity(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable Entity"
+        }), 422
+
+    # Custom error handler for other internal errors
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Internal Server Error"
+        }), 500
     
     return app
 
