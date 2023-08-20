7fcecca6dbe1677bf03f6145f99a7751a162007e
Daniil <ddreyman1@gmail.com>
2023-08-19 23:01:03 -0400

    added basic test structure

--- test_app.py
+++ test_app.py
@@ -15,7 +15,24 @@ class YourTestCase(unittest.TestCase):
     def test_create_movie(self):
         # Write a test for the POST request here
 
-    # Write more tests for other endpoints and RBAC behavior
+    def test_update_movie(self):
+        # Write a test for the PATCH request here
+
+    def test_delete_movie(self):
+        # Write a test for the DELETE request here
+
+    # Add similar test cases for actors endpoints (GET, POST, PATCH, DELETE)
+
+    # Test RBAC behavior for different roles
+    def test_get_movies_unauthorized(self):
+        response = self.app.get('/movies', headers={'Authorization': 'Bearer YOUR_INVALID_TOKEN'})
+        self.assertEqual(response.status_code, 401)
+
+    def test_create_movie_forbidden(self):
+        response = self.app.post('/movies', headers={'Authorization': 'Bearer YOUR_ACTOR_TOKEN'})
+        self.assertEqual(response.status_code, 403)
+
+    # Add similar RBAC test cases for other endpoints and roles
 
 if __name__ == '__main__':
     unittest.main()
