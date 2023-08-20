48c0ab77e9be7db33810faef3beb34154688cd30
Daniil <ddreyman1@gmail.com>
2023-08-19 23:34:21 -0400

    creat test_app.py tests for various endpoints

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
 import unittest
-from app import app
+from app import create_app
 
 class YourTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.app = create_app().test_client()
         # Set up any necessary test data or context here
 
     def test_get_movies(self):
@@ -13,15 +13,54 @@ class YourTestCase(unittest.TestCase):
         self.assertTrue('movies' in data)
 
     def test_create_movie(self):
-        # Write a test for the POST request here
+        new_movie_data = {
+            'title': 'Test Movie',
+            'release_date': '2023-08-19'
+        }
+        response = self.app.post('/movies', json=new_movie_data)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('movie' in data)
+
+        # Clean up: Delete the created movie
+        movie_id = data['movie']['id']
+        self.app.delete(f'/movies/{movie_id}')
 
     def test_update_movie(self):
-        # Write a test for the PATCH request here
+        # Create a movie first to update
+        new_movie_data = {
+            'title': 'Test Movie',
+            'release_date': '2023-08-19'
+        }
+        create_response = self.app.post('/movies', json=new_movie_data)
+        movie_id = create_response.get_json()['movie']['id']
+
+        updated_movie_data = {
+            'title': 'Updated Movie'
+        }
+        response = self.app.patch(f'/movies/{movie_id}', json=updated_movie_data)
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('movie' in data)
+
+        # Clean up: Delete the created movie
+        self.app.delete(f'/movies/{movie_id}')
 
     def test_delete_movie(self):
-        # Write a test for the DELETE request here
+        # Create a movie first to delete
+        new_movie_data = {
+            'title': 'Test Movie',
+            'release_date': '2023-08-19'
+        }
+        create_response = self.app.post('/movies', json=new_movie_data)
+        movie_id = create_response.get_json()['movie']['id']
 
-    # Add similar test cases for actors endpoints (GET, POST, PATCH, DELETE)
+        response = self.app.delete(f'/movies/{movie_id}')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('message' in data)
+
+    # I can also add similar test cases for actors endpoints (GET, POST, PATCH, DELETE) if needed
 
     # Test RBAC behavior for different roles
     def test_get_movies_unauthorized(self):
@@ -32,7 +71,7 @@ class YourTestCase(unittest.TestCase):
         response = self.app.post('/movies', headers={'Authorization': 'Bearer YOUR_ACTOR_TOKEN'})
         self.assertEqual(response.status_code, 403)
 
-    # Add similar RBAC test cases for other endpoints and roles
+    # I can also add similar RBAC test cases for other endpoints and roles if needed
 
 if __name__ == '__main__':
     unittest.main()
