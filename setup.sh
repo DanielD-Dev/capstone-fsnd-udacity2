5749a425ac1c2eaaa0b95206a25bb7e5faf9dc9b
Daniil <ddreyman1@gmail.com>
2023-08-19 23:41:18 -0400

    added correct api audience and secret

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 export AUTH0_DOMAIN='ddreyman1.us.auth0.com'
-export ALGORITHMS=['RS256']
-export API_AUDIENCE='http://localhost:5000'
+export ALGORITHMS='RS256'  # Without brackets
+export API_AUDIENCE='https://capstone_project_api.com'
 export AUTH0_CLIENT_ID='bR75Zy5DPJHhhh6mHw4k0PRRohl4Rytk'
+export AUTH0_CLIENT_SECRET='_r7NxNEt4Vd8rjUB_4efca_TtxlRiG_jbWTnRYKMoI88ujEgxSpaP3BcxMx2IJDR'
